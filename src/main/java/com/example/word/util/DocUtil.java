654227d4f88a3e36dc8a14979e9ab9422f0df839
汪永晖 <alex.yh.wang@mail.foxconn.com>
2021-03-15 16:07:41 +0800

    修改

--- src/main/java/com/example/word/util/DocUtil.java
+++ src/main/java/com/example/word/util/DocUtil.java
@@ -9,20 +9,22 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
+ * 工具类
+ *
  * @author 汪永晖
  * @Date 2021/3/15 15:23
  */
 public class DocUtil {
 
-    public static String readDoc (String filePath, InputStream is) throws IOException {
-        String text= "";
+    public static String readDoc(String filePath, InputStream is) throws IOException {
+        String text = "";
         is = FileMagic.prepareToCheckMagic(is);
         try {
             if (FileMagic.valueOf(is) == FileMagic.OLE2) {
                 WordExtractor ex = new WordExtractor(is);
                 text = ex.getText();
                 ex.close();
-            } else if(FileMagic.valueOf(is) == FileMagic.OOXML) {
+            } else if (FileMagic.valueOf(is) == FileMagic.OOXML) {
                 XWPFDocument doc = new XWPFDocument(is);
                 XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
                 text = extractor.getText();
